cbb567b007bc910bb62a777482877e0d0349b3bf
bour pauline <bourpauline@MacBook-Air-de-bour.local>
2022-10-02 14:52:02 +0200

    adding header

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
-<html>
-<h1>Hello Portfolio</h1>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="header">
+        <img class="header__logo" src="/logo.svg" />
+        <nav class="header__menu">
+            <li class="header__menu__item">Projets</li>
+            <li class="header__menu__item">Compétences & Outils</li>
+            <li class="header__menu__item">A propos</li>
+            <li class="header__menu__item">Contact</li>
+        </nav>
+
+        <div class="header__icons">
+            <img class="instagram" src="/instagram.svg" />
+            <img class="github" src="/github.svg" />
+        </div>
+    </header>
+
+</body>
+
 </html>
