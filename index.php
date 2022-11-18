a22f2c0da68c58fea34888a02525f1eae1e3a836
bour pauline <bourpauline@MacBook-Air-de-bour.local>
2022-11-18 17:21:45 +0100

    styling index.css & adding project pages

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css">
     <title>Document</title>
 </head>
 <body>
@@ -18,7 +19,7 @@
 <!-- Overlay content -->
 <div class="overlay-content">
             <a href="#projects" class="header__menu__item">Projets</a>
-            <a href="#" class="header__menu__item">Compétences & Outils</a>
+            <a href="#block__skills" class="header__menu__item">Compétences & Outils</a>
             <a href="#" class="header__menu__item">A propos</a>
             <a href="#" class="header__menu__item">Contact</a>
             <a href="https://www.instagram.com/somewhereonlywedraw/" target="blank"><img class="instagram" src="/instagram.svg" /></a>
@@ -26,11 +27,12 @@
 </div>
 
 </div>
+
     <header class="header"> 
         <img class="header__logo" src="/logo.svg" />
         <nav class="header__menu">
             <a href="#projects" class="header__menu__item">Projets</a>
-            <a href="#" class="header__menu__item">Compétences & Outils</a>
+            <a href="#block__skills" class="header__menu__item">Compétences & Outils</a>
             <a href="#" class="header__menu__item">A propos</a>
             <a href="#" class="header__menu__item">Contact</a>
         </nav>
@@ -49,84 +51,128 @@
         </div>
     </header>
     <div class="block__presentation">
-        <img class="" src="/pauline-profil.png"/>
+        <img class="profil__picture" src="/pauline-profil.png"/>
         <div class="block__presentation__introduction">
         <p class="block__presentation__title">Hey ! C’est Pauline </p>
-        <p class="block__presentation__description">Webdesigner & Intégratrice, très intéréssée par le domaine de l’illustration, l’animation, et le développement web, j’adore apprendre et cherche à acquérir de nouvelles compétences !</p>
+        <div class="block__presentation__description">
+        <p class="">Webdesigner & Intégratrice, très intéréssée par le domaine de l’illustration, l’animation, et le développement web, j’adore apprendre et cherche à acquérir de nouvelles compétences !</p>
+        <p>Ne soyez pas timide...</p>
+        <a href="/cv-Bour-Pauline.pdf" class="button-52" role="button" target="blank">Téléchargez mon CV !</a>
         </div>
+      </div>
     </div>
 
-    <div class="block__expertises">
-      <div class="block__expertises__item">
-        <p>Webdesign</p>
+    <svg width="100%" height="94" viewBox="0 0 1920 94" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 42.015L32 42.4365C64 42.9985 128 43.8415 192 36.8165C256 29.7915 320 14.8985 384 14.8985C448 14.8985 512 29.7915 576 39.6265C640 49.4615 704 54.2385 768 50.8665C832 47.635 896 36.395 960 31.4775C1024 26.56 1088 27.965 1152 21.9235C1216 15.7415 1280 2.25351 1344 0.286513C1408 -1.53999 1472 8.29501 1536 19.1135C1600 29.7915 1664 41.5935 1728 47.354C1792 53.255 1856 53.255 1888 53.255H1920V95.405H1888C1856 95.405 1792 95.405 1728 95.405C1664 95.405 1600 95.405 1536 95.405C1472 95.405 1408 95.405 1344 95.405C1280 95.405 1216 95.405 1152 95.405C1088 95.405 1024 95.405 960 95.405C896 95.405 832 95.405 768 95.405C704 95.405 640 95.405 576 95.405C512 95.405 448 95.405 384 95.405C320 95.405 256 95.405 192 95.405C128 95.405 64 95.405 32 95.405H0V42.015Z" fill="#A4BBA9"/>
+</svg>
+    <div id="block__skills">
+    <div class=" container">
+      <p class="skills__title title">Mes compétences & outils</p>
+      <div class="block__skills__wrapper">
+        <div class="icon__box">
+        <i class="devicon devicon-html5-plain"></i>
+        <p>HTML</p>
+        </div>
+        <div class="icon__box">
+        <i class="devicon devicon-css3-plain"></i>
+        <p>CSS</p>
+        </div>
+        <div class="icon__box">
+        <div class="container-img">
+        <img src="/prestashop.png"/>
+        </div>
+        <p>Prestashop</p>
+        </div>
+        <div class="icon__box">
+        <i class="devicon devicon-wordpress-plain"></i>
+        <p>Wordpress</p>
+        </div>
+        <div class="icon__box">
+        <i class="devicon devicon-figma-plain"></i>
+        <p>Figma</p>
+        </div>
+        <div class="icon__box">
+        <i class="devicon devicon-xd-plain"></i>
+        <p>Adobe XD</p>
+        </div>
+        <div class="icon__box">
+        <i class="devicon devicon-photoshop-plain"></i>
+        <p>Photoshop</p>
+        </div>
+        <div class="icon__box">
+        <i class="devicon devicon-illustrator-plain"></i>
+        <p>Illustrator</p>
+        </div>
+        <div class="icon__box">
+        <div class="container-img">
+        <img src="/indesign.png"/>
+        </div>
+        <p>InDesign</p>
+        </div>
+        <div class="icon__box">
+        <i class="devicon devicon-aftereffects-plain"></i>
+        <p>After Effect</p>
+        </div>
+        <div class="icon__box">
+        <div class="container-img">
+        <img src="/prestashop.png"/>
+        </div>
+        <p>Procreate</p>
+        </div>
       </div>
-      <div class="block__expertises__item">
-        <p>Illustration</p>
-      </div>
-      <div class="block__expertises__item">
-        <p>Intégration</p>
-      </div>
-      <div class="block__expertises__item">
-        <p>Motion design</p>
-      </div>
+    </div>
     </div>
 
     <div class="container">
     <div id="projects">
       <div class="project__header">
-      <p>Quelques récents projets</p> 
-      <div class="projects__menu">
-      <a href="#" class="projects__menu__item">Tous les projets</a>
-      <a href="#" class="projects__menu__item">Webdesign</a>
-      <a href="#" class="projects__menu__item">Intégration</a>
-      <a href="#" class="projects__menu__item">Illustration</a>
-      <a href="#" class="projects__menu__item">Motion design</a>
+      <p class="title">Quelques récents projets</p> 
       </div>
+      <div class="projects__section">
+        <div class="projects__section__item">
+          <a href="/webdesign.html">
+          <img src="/ex-projet.png"/>
+          <div class="projects__section__item__title">
+          <p>Maquettage</p>
+          </div>
+          <p class="projects__section__item__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+          </a>
+        </div>
+        <div class="projects__section__item">
+        <a href="/illustration.html">
+          <img src="/ex-projet.png"/>
+          <div class="projects__section__item__title">
+          <p>Illustration</p>
+          </div>
+          <p class="projects__section__item__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+        </a>
+        </div>
+        <div class="projects__section__item">
+        <a href="/logo.html">
+          <img src="/ex-projet.png"/>
+          <div class="projects__section__item__title">
+          <p>Branding / Logo</p>
+          </div>
+          <p class="projects__section__item__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+        </a>
+        </div>
       </div>
       <div class="projects__section">
         <div class="projects__section__item">
           <img src="/ex-projet.png"/>
           <div class="projects__section__item__title">
-          <p>PROJECT NAME</p>
+          <p>Code</p>
           </div>
           <p class="projects__section__item__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
         </div>
         <div class="projects__section__item">
+        <a href="/motion-design.html">
           <img src="/ex-projet.png"/>
           <div class="projects__section__item__title">
-          <p>PROJECT NAME</p>
+          <p>Motion design</p>
           </div>
           <p class="projects__section__item__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
-        </div>
-        <div class="projects__section__item">
-          <img src="/ex-projet.png"/>
-          <div class="projects__section__item__title">
-          <p>PROJECT NAME</p>
-          </div>
-          <p class="projects__section__item__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
-        </div>
-      </div>
-      <div class="projects__section">
-        <div class="projects__section__item">
-          <img src="/ex-projet.png"/>
-          <div class="projects__section__item__title">
-          <p>PROJECT NAME</p>
-          </div>
-          <p class="projects__section__item__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
-        </div>
-        <div class="projects__section__item">
-          <img src="/ex-projet.png"/>
-          <div class="projects__section__item__title">
-          <p>PROJECT NAME</p>
-          </div>
-          <p class="projects__section__item__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
-        </div>
-        <div class="projects__section__item">
-          <img src="/ex-projet.png"/>
-          <div class="projects__section__item__title">
-          <p>PROJECT NAME</p>
-          </div>
-          <p class="projects__section__item__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+        </a>
         </div>
       </div>
     </div>
