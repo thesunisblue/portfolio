489276f3e78bb554a47999ccbfe1215a8e09684d
thesunisblue <paulinebour15@gmail.com>
2024-03-14 19:48:41 +0100

    correction titres de pages

--- webdesign.php
+++ webdesign.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
     <link rel="stylesheet" href="projet.css">
-    <title>Document</title>
+    <title>Webdesign - Popscreativestudio</title>
 </head>
 <body>
 <div id="myNav" class="overlay">
