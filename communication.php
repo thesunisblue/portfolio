ac643d0044fc63dcbee79b045e4ceb8af3674b9e
thesunisblue <paulinebour15@gmail.com>
2024-11-27 15:55:42 +0100

    ajout page

---
+++ communication.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="projet.css">
+    <title>Communication - Popscreativestudio</title>
+</head>
+<body>
+<?php require "header.php"; ?>
+
+Bientôt disponible :)
+
+<?php require "footer.php"; ?>
+
+</body>
+</html>
